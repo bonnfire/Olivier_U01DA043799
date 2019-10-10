3461a2d0454e0371daf65ebcd2a35e4f748c8187
Bonnie Lin <b2lin@ucsd.edu>
2019-10-10 15:24:19 -0700

    Troubleshoot assignment for loop, Date columns populating correctly and correctly extracted to other datasets, created dead, switches tables

--- Olivier_function_extractandprocess.R
+++ Olivier_function_extractandprocess.R
@@ -5,6 +5,11 @@ library(data.table)
 library(tidyverse)
 library(readxl)
 
+install.packages('splitstackshape')
+install.packages('janitor')
+library(splitstackshape)
+library(janitor)
+
 olivierfiles <- function(filename){
   options(scipen = 100) # prevent sci notation
   u01.importxlsx <- function(xlname){
@@ -13,15 +18,26 @@ olivierfiles <- function(filename){
     return(df)
   }
   
+  ## XXXXX REMOVE after troubleshooting filename = "C08_cocaine.xlsx"
   selfadmin <- u01.importxlsx(filename)[[1]] %>%
     as.data.table %>% 
     na.omit(cols = seq_along('Rat'))
   
   # turn all into character for preservation in transpose
   selfadmin[, names(selfadmin) := lapply(.SD, as.character)] 
+  if(any(grepl("^[[:digit:]]{5,}", selfadmin$Date))){
+    selfadmin$Date <- as.character(as.POSIXct(as.numeric(selfadmin$Date) * (60*60*24), origin="1899-12-30", tz="UTC", format="%Y-%m-%d"))
+  } # convert Excel character into dates
+  
+  # fix missing column names
+  if(any(grepl("REWARDS" , names(selfadmin)))){
+    setnames(selfadmin, old = c("REWARDS"), new = c("Rat")) # prepares for next code
+  }
+  names(selfadmin)[length(names(selfadmin))] = "FLAG" # the last column should be FLAG
   
   # extract experiment name (ShA and LgA)
-  selfadmin$Rat <- ifelse(grepl("^\\D{2}A", selfadmin$Rat), as.character(stringr::str_match(selfadmin$Rat,"..A")), selfadmin$Rat)
+  selfadmin$Rat <- ifelse(grepl("^\\D{2}A", selfadmin$Rat), as.character(stringr::str_match(selfadmin$Rat,"..A")), selfadmin$Rat) # reg exp fixes issuse of extracting mA
+  selfadmin$Rat <- gsub(" |[(]|[)]|-", "", selfadmin$Rat) # remove all unwanted characters
   
   # make experiment name unique (# code from G. Grothendieck (Stack Overflow) )
   uniquify <- function(x) if (length(x) == 1) x else sprintf("%s%02d", x, seq_along(x)) 
@@ -39,30 +55,64 @@ olivierfiles <- function(filename){
   nm <- names(tselfadmin)[-1] # use these columns to make date columns
   nm1 <- paste("date", nm, sep = "_") # make these date columns
   tselfadmin[ , ( nm1 ) := lapply( .SD, function(x) c(grep("^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$", x, value = T)) ) ,  .SDcols = nm ]
-  ind <- grep("^(date)", names(tselfadmin), perl = T) # bring dates back to posixct
+  ind <- grep("^(date)", names(tselfadmin), perl = T) # bring date characters back to posixct
   for (i in seq_along(ind)) {
     set(tselfadmin, NULL, ind[i], as.POSIXct(tselfadmin[[ind[i]]], tz = "UTC"))
   }
   tselfadmin <- tselfadmin[!grepl("^\\d{4}\\-", sha01),] # remove the row with dates
   
+  # extract [*DATA DICTIONARY*]
+  datadictionary <- selfadmin[, 1:3] # vertical formatting is preferred in selfadmin
+  tselfadmin <- tselfadmin[-c(1:2), ] # remove data dictionary from data
+  
   # add general comments columns
-  rownumber <- which(is.na(tselfadmin$rfid)) %>% head(1)
-  colswithcomments <- colnames(tselfadmin)[which(!is.na(tselfadmin[rownumber,]))] # return columns that have comments
+  # remove rows under rfid that don't have comments
+  # rownumber <- which(is.na(tselfadmin$rfid)) %>% head(1) 
+  
+  # remove <- which(grepl("^[[:alpha:]]", tselfadmin$rfid)) %>% head(1) # define remove cases
+  #   if(length(remove) != 0){
+  #   tselfadmin <- tselfadmin[-remove, ]
+  #   }
+  
+  rownumber <- which(is.na(tselfadmin$rfid)|grepl("^[^0-9]", tselfadmin$rfid)) %>% head(1)
+  colswithcomments <- colnames(tselfadmin)[which(!is.na(tselfadmin[rownumber, ]))] # return columns that have comments
   colswithcomments <- grep("^(?!date)", colswithcomments, perl = T, value = T) # return non-date columns that have comments
   nm2 <- paste("comment", colswithcomments, sep = "_")
   tselfadmin[ , ( nm2 ) := lapply( .SD, function(x) c(grep("^.", x[rownumber], value = T))) ,  .SDcols = colswithcomments ]
   tselfadmin <- tselfadmin[-rownumber,] # remove the row with general comments
-  
-  # extract [*DATA DICTIONARY*]
-  datadictionary <- selfadmin[, 1:3] # vertical formatting is preferred in selfadmin
-  tselfadmin <- tselfadmin[-c(1:2), ] # remove data dictionary from data
+
   
   # extract [*TABLE FOR SPECIFIC COMMENTS*]
   rownumbers <- which(is.na(tselfadmin$rfid))
   colswithspeccomments <- colnames(tselfadmin)[which(!is.na(tselfadmin[rownumbers[1],]))] # return columns that have comments
   colswithspeccomments <- grep("^(?![date|comment])", colswithspeccomments, perl = T, value = T) # further prune columns that don't contain comment or date
   specificcomments <- tselfadmin[rownumbers, ..colswithspeccomments] # extract special comments
-  tselfadmin <- tselfadmin[-rownumbers, -..colswithspeccomments] # remove the special comments
+  specificcommentsdates <- sapply(colswithspeccomments, grep, names(tselfadmin), value = T)
+  specificcommentsdates <- sapply(specificcommentsdates, "[", 2) %>% as.character # extract the date column name from every item in list
+  specificcommentsdates <- tselfadmin[1, ..specificcommentsdates] ## get dates
+  tselfadmin <- tselfadmin[-rownumbers, ] # remove the special comments
+  
+  # reshape TABLE FOR SPECIFIC COMMENTS
+  specificcomments <- rbindlist(list(specificcomments, as.list(names(specificcomments))), fill=FALSE) # preserve experiment name as rows before tranposing
+  tspecificcomments <- data.table::transpose(specificcomments)
+  tspecificcomments <- janitor::remove_empty(tspecificcomments, "cols") # remove empty columns
+  specificcommentsdateslg <- melt(specificcommentsdates, variable.name = "experiment", value.name = "date")  # prepare dates # melt from wide to long 
+  specificcommentsdateslg$experiment <- gsub("date_", "", specificcommentsdateslg$experiment)
+  setnames(tspecificcomments, c("conflict", "comment", "flag", "experiment"))
+  setkey(tspecificcomments, cols = experiment) # set key to prepare merge 
+  tspecificcomments <- tspecificcomments[specificcommentsdateslg] # specific comments now has date info 
+  tspecificcomments <- splitstackshape::cSplit(tspecificcomments, splitCols = "conflict", sep = ";", direction = "long") # breaks multiple cases in one entry into numerous rows
+  tspecificcomments[, c("labanimalid", "conflict") := tstrsplit(conflict, ":", fixed=TRUE)] # data table splits text string
+  tspecificcomments[, ("labanimalid") := sapply(.SD, function(x) c(stringr::str_extract(x, "[[:alnum:]]+[[:digit:]]+$"))) , .SDcols = "labanimalid"] # clean data
+  tspecificcomments[, conflict := str_trim(conflict)] # should remove trailing whitespace
+  
+  # extract [*TABLE FOR COMPROMISED ANIMALS*]
+  
+  # extract [*TABLE FOR DEAD ANIMALS*]
+  dead <- tspecificcomments[conflict %like% "(?i)died|dead"] # data.table %ilike% operator doesn't seem ready so using (?i) tag for case insensitive searches
+  
+  # extract [*TABLE FOR SWITCHED ANIMALS*]
+  switches <- tspecificcomments[conflict %like% "(?i)switch(ed)?"]
   
   # add cohort column
   tselfadmin <- append(tselfadmin, list(data.table(cohort = sub("_cocaine.*", "", filename)))) %>% as.data.table   # get from file name
@@ -84,7 +134,9 @@ olivierfiles <- function(filename){
 
   list <- list("tselfadmin" = tselfadmin,
                "datadictionary" = datadictionary,
-               "specificcomments" = specificcomments)
+               "specificcomments" = tspecificcomments,
+               "dead" = dead,
+               "switches" = switches)
   
   return(list)
 } 
@@ -93,34 +145,24 @@ olivierfiles <- function(filename){
 cohortfiles <- list.files(pattern = "*.xlsx")
 # cohort 6 is missing
 for(i in 1:length(cohortfiles)){
-  assign(paste0("cohort", i + 1), olivierfiles(cohortfiles[i]))
-  if(i == 4) next 
+  # if(i == 4) next 
   if(i > 5){
     assign(paste0("cohort", i + 1), olivierfiles(cohortfiles[i]))
-  } 
+  } else
+    assign(paste0("cohort", i), olivierfiles(cohortfiles[i]))
 }
 
 
 
 # function breaks at 
-# 4, 8
-cohort4 <-  olivierfiles("C04_ cocaine.xlsx") # 4 breaks, =
-#
+cohort2 <- olivierfiles("C02_cocaine.xlsx")
+cohort3 <- olivierfiles("C03_cocaine.xlsx")
+cohort4 <- olivierfiles("C04_cocaine.xlsx")
 cohort5 <- olivierfiles("C05_cocaine.xlsx") 
 cohort7 <- olivierfiles("C07_cocaine.xlsx")
 cohort8 <- olivierfiles("C08_cocaine.xlsx") # look into why its called REWARDS, throwing off the code from Rat default
 
-cohort5 <- u01.importxlsx("C05_cocaine.xlsx")[[1]]
-
-u01.importxlsx("C05_cocaine.xlsx")
-"C08_cocaine.xlsx"  
-
-selfadmin <- cohort4ma %>%
+filename <- "C02_cocaine.xlsx"
+selfadmin <- u01.importxlsx("C01_cocaine.xlsx")[[1]] %>%
   as.data.table %>% 
   na.omit(cols = seq_along('Rat'))
-
-
-selfadminRat <- selfadmin$Rat
-selfadminRat <- c(selfadminRat, "Date_ShA01", "Date_ShA02", "Comment_ShA01", "LabanimalID", "Cohort")
-ind <- grep(pattern = "^(?!RFID | Date | Comment | LabanimalID | Cohort)", selfadminRat, perl = T, value = T)
-
