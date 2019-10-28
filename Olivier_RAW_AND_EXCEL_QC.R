6cafcce0cb0d291bf848fde18d2cafb91d446c2c
Bonnie Lin <b2lin@ucsd.edu>
2019-10-28 16:12:49 -0700

    gwas template setup, waiting for confirmation from lab about important variables

--- Olivier_RAW_AND_EXCEL_QC.R
+++ Olivier_RAW_AND_EXCEL_QC.R
@@ -29,14 +29,21 @@ selfadmindatabasexl <- GWAS_database_xl[[1]]
 selfadmindatabasexl_subset <- selfadmindatabasexl %>% 
   select(`RAT #`, `Acquis ShA`:`SHOCK`) %>%
   rename_all(tolower) # XX REMOVE SPACES FROM VARNAMES 
-
+names(selfadmindatabasexl_subset) <- gsub(selfadmindatabasexl_subset, )
 
 selfadmin_df_gwascopy <- selfadmin_df %>% 
   mutate(avgsha7_10 = rowMeans(.[, sha07:sha10]),
          avglga11_14 = rowMeans(.[, lga11:lga14]),
+         avg_zlga11_14 = 
          ) %>% # rowMeans(a[c('high', 'low')], na.rm=TRUE) XX PICK UP HERE -- NOT SURE IF NA SHOULD BE REMOVED FROM MEAN CALCULATIONS
   mutate(`acquis_sha` = ifelse(avgsha7_10 < 4, "No", "Yes"),
-         `escalation` = ifelse(avglga11_14 > 4, "Vulnerable", "Resistant"))
+         `escalation` = ifelse(avglga11_14 > 4, "Vulnerable", "Resistant")) %>% 
+  group_by(sex) %>% 
+  mutate(sex_mean = _ - mean(), 
+         sex_sd = sd()) %>% 
+  ungroup() %>%
+  mutate(add_ind = (maxpr - sex_mean)/sex_df,
+         add_ind_cat = ifelse(addind>0.49|addind==0.49, "High AI", "Low AI")
 # `add ind` =IF(BW2>=0.49,"High AI","Low AI")
 # `fr` =IF(BT2>=0.49,"High FR","Low FR")
 # `pr` =IF(BU2>=0.49,"High PR","Low PR")
@@ -44,12 +51,15 @@ selfadmin_df_gwascopy <- selfadmin_df %>%
 
 rowMeans(a[c('high', 'low')], na.rm=TRUE)
 
-## TO DO:  
+
+# check ids from the raw file, find the four 14 digit id's (should be 15)
+
+
+## DONE: (see documentation in u01_spleenceca_shipment.R) 
 # check if spleen and ceca are in the naive
 # extract naive ids from wfu 
 # extract ids from the spleen and ceca shipment files 
 # check if matches 
 
 setwd("~/Dropbox (Palmer Lab)/Palmer Lab/Bonnie Lin/20190829_WFU_U01_ShippingMaster")
-# extract the naive cases from the wfu data
-# 
+# extract the naive cases from the wfu data in u01_qc.R
