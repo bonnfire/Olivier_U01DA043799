0d50202e7f59aed66ded351ef9278f0d0cf60da3
Bonnie Lin <b2lin@ucsd.edu>
2019-12-09 16:15:44 -0800

    create timestamps long format

--- CREATE/EXTRACT_RAW.R
+++ CREATE/EXTRACT_RAW.R
@@ -18,9 +18,30 @@ rewardstest_split <- split(rewardstest[[1]], cumsum(1:nrow(rewardstest[[1]]) %in
 
 
 readsubjects <- function(x){
-subjects <- fread(paste0("awk '/Subject/{print $2}' ", "'", x, "'"), fill = T)
+subjects <- fread(paste0("awk '/Subject/{print $2}' ", "'", x, "'"),fill = T,header=F)
 return(subjects)
 }
 
 
 names(rewardstest_split) <- lapply(olivier_cocaine_files[2], readsubjects) %>% rbindlist() %>% unlist() %>% as.character()
+lapply(olivier_cocaine_files[2], readsubjects)
+
+
+readrightresponses <- function(x){
+  subjects <- fread(paste0("awk '/Subject/{print $2}' ", "'", x, "'"),fill = T,header=F)
+  return(subjects)
+}
+
+readrightresponses_time <- function(x){
+  rightresponses <- fread(paste0("awk '/Y:/{flag=1;next}/^$/{flag=0}flag' ", "'", x, "'"), fill = T) # matching to empty lines
+  return(rightresponses)
+}
+rightresponses_time <- lapply(olivier_cocaine_files[2], readrightresponses_time)
+rightresponses_time_indices <- grep("^0:$", rightresponses_time[[1]]$V1)
+rightresponses_time_split <- split(rightresponses_time[[1]], cumsum(1:nrow(rightresponses_time[[1]]) %in% rightresponses_time_indices))
+names(rightresponses_time_split) <- lapply(olivier_cocaine_files[2], readsubjects) %>% rbindlist() %>% unlist() %>% as.character()
+gdata::unmatrix(rightresponses_time_split[[1]])
+
+test <- rightresponses_time_split[[1]][,-1]
+test <- test[rowSums(test[, -1] > 0) != 0, ]
+data.frame(timestamps = as.vector(data.matrix(test))) %>% head
