3271af509cdc49a8033c0ca9e8f65746a9d9b764
Bonnie Lin <b2lin@ucsd.edu>
2020-06-09 10:53:59 -0700

    write pipeline basic model for writing ataframes to database

--- CREATE/R_sql_pgdump.R
+++ CREATE/R_sql_pgdump.R
@@ -6,33 +6,21 @@ library(RPostgreSQL)
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, user='postgres', password='postgres', dbname='U01')
 
+## PICK UP AND REDO BC ALL OF THE REWARDS ARE NA
 
-# drop table if it already exists
-# if (dbExistsTable(con, c("u01_tom_jhou","jhou_wfu_master")))
-#   dbRemoveTable(con, c("u01_tom_jhou","jhou_wfu_master"))
 
 # write data frame to database
-
-Olivier_Cocaine_df_sql$cohort %>% table()
-test1 <- Olivier_Cocaine_df_sql %>% subset(cohort != "C11") 
-test2 <- Olivier_Cocaine_df_sql %>% subset(cohort == "C11")
+## to add dataframes (by cohort)
+## write to a temporary table in the database with dbWriteTable
+# then issue an SQL statement to insert into your table as you would within the db environment
+test1 <- Olivier_Cocaine_df_sql %>% distinct() %>% subset(cohort != "C11") 
+test2 <- Olivier_Cocaine_df_sql %>% distinct() %>% subset(cohort == "C11")
 dbWriteTable(con, c("u01_olivier_george_cocaine","olivier_rewards"), value = test1, row.names = FALSE)
-append_sql <- sqlAppendTable(con, c("u01_olivier_george_cocaine","olivier_rewards"), test2, row.names = F) 
-dbExecute(con, append_sql)
-dbExecute(con, "INSERT INTO u01_olivier_george_cocaine.olivier_rewards (cohort, rfid, labanimalid, exp, rewards) VALUES ('C11', '933000320187309', NULL, 'SHA08', NULL)")
+dbWriteTable(con, c("u01_olivier_george_cocaine","olivier_rewards_temp"), value = test2, row.names = FALSE)
+dbExecute(con,"ALTER TABLE u01_olivier_george_cocaine.olivier_rewards ADD PRIMARY KEY(rfid,exp)")
+dbExecute(con,"insert into u01_olivier_george_cocaine.olivier_rewards select * from u01_olivier_george_cocaine.olivier_rewards_temp")
+dbExecute(con,"drop table if exists u01_olivier_george_cocaine.olivier_rewards_temp")
 
-upd_sql <- "UPDATE olivier_rewards
-            SET t.Val = s.Val
-            FROM dbo.tbl t 
-            INNER JOIN dbo.tbl_staging s 
-               ON t.[Key] = s.[Key]"
-
-dbSendQuery(con, upd_sql)
-
-sql <- "
-    select rewards
-    from u01_olivier_george_cocaine.olivier_rewards
-    where cohort='C01'
-"
-results <- dbGetQuery(con, sql)
+# for troubleshooting 
+dbExecute(con,"drop table if exists u01_olivier_george_cocaine.olivier_rewards")
 
