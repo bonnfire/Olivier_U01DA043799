f2e154d79daf90adfc7e03996b41d2136790f018
Bonnie Lin <b2lin@ucsd.edu>
2020-11-25 14:47:40 -0800

    create xl for lga to presses

--- CREATE/EXTRACT_RAW.R
+++ CREATE/EXTRACT_RAW.R
@@ -1201,6 +1201,7 @@ lga_c01_11_timeout <- lapply(lga_c01_11_files, function(x){
   if(grepl("Old", x)){ # process if directory is old
     df <- fread(paste0("awk '/^RatNumber/{flag=1;next}/^ProgramName/{flag=0}flag' ", "'", x, "'"), fill = T, header = F) 
     df$filename = x
+    df$box = fread(paste0("awk '/^BoxNumber/{flag=1;next}/^Sessionlength/{flag=0}flag' ", "'", x, "'"), fill = T, header = F)  
     df$to_active_presses = fread(paste0("awk '/^TotalTOResponses/{flag=1;next}/^TotalRspInAct/{flag=0}flag' ", "'", x, "'"), fill = T, header = F)
   }
   
@@ -1209,8 +1210,9 @@ lga_c01_11_timeout <- lapply(lga_c01_11_files, function(x){
     df$filename = x 
     df$rewards = fread(paste0("awk '/B:/{print $2}' ", "'", x, "'"), fill = T, header = F)
     df$presses = fread(paste0("awk '/G:/{print $2}' ", "'", x, "'"), fill = T, header = F)
+    df$box = fread(paste0("awk '/Box:/{print $2}' ", "'", x, "'"), fill = T, header = F) 
     df$to_active_presses =  df$presses -  df$rewards
-    df <- df[, c("V1", "filename", "to_active_presses")]
+    df <- df[, c("V1", "filename", "box", "to_active_presses")]
   }
   
   return(df)
@@ -1226,16 +1228,63 @@ lga_c01_11_timeout_df <- lga_c01_11_timeout %>% rbindlist() %>%
   mutate(labanimalid = str_extract(toupper(labanimalid), "[MF]\\d+"),
          session = str_extract(toupper(filename), "LGA\\d+"),
          cohort = str_extract(toupper(filename), "/C\\d+/") %>% gsub("/", "", .),
-         sex = str_extract(toupper(labanimalid), "[MF]")
+         sex = str_extract(toupper(labanimalid), "[MF]"),
+         room = ifelse(grepl("[[:alnum:]]+C\\d{2}HS", filename), gsub("C\\d{2}HS.*", "", filename) %>% gsub(".*LGA/", "", .), NA)
   ) %>% 
-  select(cohort, labanimalid, sex, session, to_active_presses, filename)
+  select(cohort, labanimalid, sex, session, box, room, to_active_presses, filename) 
+
+lga_c01_11_timeout_trials1_14 <- lga_c01_11_timeout_df %>% 
+  subset(parse_number(session) < 15) %>% 
+  mutate(box = as.character(box))
 
 # fix these 
-lga_c01_11_timeout_df %>% get_dupes(labanimalid, session)
+lga_c01_11_timeout_trials1_14 %>% get_dupes(labanimalid, session)
+# fix the na subject with boxes 
+lga_c01_11_timeout_trials1_14_join <- lga_c01_11_timeout_trials1_14 %>% 
+  left_join(box_metadata_long %>% 
+              select(cohort, exp, box, room, labanimalid) %>% 
+              rename("session" = "exp"), by = c("cohort", "room", "box", "session")) %>% 
+  mutate(labanimalid = coalesce(labanimalid.x, labanimalid.y)) %>% 
+  select(-labanimalid.x, -labanimalid.y) 
+# lga_c01_11_timeout_trials1_14_join %>% get_dupes(labanimalid, session) %>% naniar::vis_miss
+lga_c01_11_timeout_trials1_14_distinct <- lga_c01_11_timeout_trials1_14_join %>% 
+  select(cohort, labanimalid, sex, session, box, room, to_active_presses) %>% 
+  distinct() %>% 
+  left_join(lga_c01_11_timeout_trials1_14_join %>% 
+              select(cohort, labanimalid, sex, session, box, room, to_active_presses) %>% 
+              distinct() %>% 
+              get_dupes(labanimalid, session) %>% 
+              group_by(labanimalid, session, .drop = F) %>% 
+              mutate(nzero = sum(to_active_presses)) %>% 
+              subset(to_active_presses != nzero&to_active_presses == 0) %>% ungroup() %>% mutate(drop = "drop") %>% 
+              select(labanimalid, cohort, session, to_active_presses, drop), by = c("labanimalid", "cohort", "session", "to_active_presses")) %>% 
+  subset(is.na(drop))
+lga_c01_11_timeout_trials1_14_brent <- lga_c01_11_timeout_trials1_14_distinct %>% 
+  left_join(., lga_c01_11_timeout_trials1_14_distinct %>% get_dupes(labanimalid, session) %>% select(labanimalid, cohort, session, to_active_presses, dupe_count), by = c("labanimalid", "cohort", "session", "to_active_presses")) %>% 
+  rename("need_check"="dupe_count") %>% 
+  mutate(need_check = ifelse(!is.na(need_check), "check", NA)) %>% 
+  select(cohort, labanimalid, sex, session, box, room, to_active_presses, need_check) %>% 
+  mutate(labanimalid_num = parse_number(labanimalid)) %>% 
+  arrange(cohort, sex, labanimalid_num, session) %>% 
+  select(-labanimalid_num)
 
 
-
-
+write.xlsx(lga_c01_11_timeout_trials1_14_brent, "~/Dropbox (Palmer Lab)/Palmer Lab/Bonnie Lin/github/Olivier_U01Cocaine/CREATE/cocaine_lga_to_presses.xlsx")
+# subset(!(labanimalid %in% (lga_c01_11_timeout_trials1_14_join %>% 
+#                              select(cohort, labanimalid, sex, session, box, room, to_active_presses) %>% 
+#                              distinct() %>% 
+#                              get_dupes(labanimalid, session) %>% 
+#                              group_by(labanimalid, session, .drop = F) %>% 
+#                              mutate(nzero = sum(to_active_presses)) %>% 
+#                              subset(to_active_presses == nzero) %>% ungroup() %>% distinct(labanimalid) %>% unlist() %>% as.character)&to_active_presses %in% (lga_c01_11_timeout_trials1_14_join %>% 
+#                                                                                                                                                                select(cohort, labanimalid, sex, session, box, room, to_active_presses) %>% 
+#                                                                                                                                                                distinct() %>% 
+#                                                                                                                                                                get_dupes(labanimalid, session) %>% 
+#                                                                                                                                                                group_by(labanimalid, session, .drop = F) %>% 
+#                                                                                                                                                                mutate(nzero = sum(to_active_presses)) %>% 
+#                                                                                                                                                                subset(to_active_presses == nzero) %>% 
+#                                                                                                                                                                ungroup() %>% select(to_active_presses) %>% unlist() %>% as.character)))
+# lga_c01_11_timeout_trials1_14_distinct %>% get_dupes(labanimalid, session)
 
 #####
 ## sha
